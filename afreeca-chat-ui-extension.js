e5e570e8c05fef26fe97293f31e3c5caecb63418
youngeun.ahn <keyfrebern@gmail.com>
2024-03-17 20:17:50 +0900

    chore: Tampermonkey 스크립트 업데이트 테스트

--- afreeca-chat-ui-extension.js
+++ afreeca-chat-ui-extension.js
@@ -12,6 +12,8 @@
 (() => {
     'use strict'
 
+    console.log('test')
+
     // Data
     const [, broadName, broadNo] = location.href.split('/')
     const broadNickname = $('.nickname').eq(0).attr('title')
