7a9cbd013921944bb152f5cd276de2cf8dade487
youngeun.ahn <keyfrebern@gmail.com>
2024-03-18 13:56:19 +0900

    feat: 최근 사용한 이모티콘 목록 구현

--- afreeca-chat-ui-extension.js
+++ afreeca-chat-ui-extension.js
@@ -22,6 +22,9 @@
   const boxChat = $('#chat_write')
   const boxChatArea = $('#chat_area')
   const btnBalloon = $('#btn_star')
+  const btnAddSkip = $('#da_btn_skip')
+  const btnEmoticon = $('#btn_emo > a').eq(0)
+  const boxEmoticon = $('<div>')
 
   const labelBalloonCount = $('<label for="btn_star">000</label>')
     .on('click', () => btnBalloon.children().eq(0).click())
@@ -33,7 +36,7 @@
     headers: { 'Cookie': document.cookie },
   })
 
-  // 보유 별풍선 개수
+  // 보유 별풍선 개수 요청
   async function fetchBalloonCount () {
     const form = {
       nBroadNo: broadNo,
@@ -62,10 +65,10 @@
     return max
   }
 
+  // 복사/잘라내기/붙여넣기 우회 구현(콘은 복사 불가)
   function onKeyDownChatInput (e) {
     const selection = window.getSelection()
 
-    /* 복붙 방지 우회 구현(콘은 복사 불가) */
     if (e.ctrlKey && e.key === 'c') {
       const content = selection.toString()
       navigator.clipboard.writeText(content)
@@ -108,6 +111,16 @@
     inputChat.on('keydown', onKeyDownChatInput)
     inputChat.on('focus', fetchBalloonCount)
 
+    boxEmoticon.css({
+      position: 'absolute',
+      top: '0.8rem',
+      right: '0.8rem',
+      height: '2.4rem',
+      display: 'flex',
+      gap: '0.4rem',
+    }).attr('title', '최근에 사용한 이모티콘')
+    boxEmoticon.insertBefore(boxChat)
+
     // 불필요한 UI 제거
     $('#btn_sticker').hide()
     $('#btn_police').hide()
@@ -115,6 +128,8 @@
     fetchBalloonCount()
   }
 
+  // 로그인/로그아웃 등으로 userId가 변경되었을 때
+  // 로그인 ID 및 별풍선 갱신
   function watchUserIdInterval () {
     loginId = getCookie('PdboxBbs')
 
@@ -125,16 +140,13 @@
         fetchBalloonCount()
         loginId = nextLoginId
       }
-    }, 500)
+    }, 1000)
   }
 
+  // 첫 광고 시간 지나면 자동 스킵 기능
   function setAutoAddSkipInterval () {
-    const btnAddSkip = $('#da_btn_skip')
-
     let count = 0
-    let interval = setInterval(() => {
-      $('#chat_ad').hide()
-
+    const interval = setInterval(() => {
       // 60초 후에는 그냥 clear
       count++
       if (count === 60) {
@@ -149,12 +161,42 @@
     }, 1000)
   }
 
+  // 최근 사용한 이모티콘 목록
+  function initRecentEmoticons () {
+    function updateRecentEmoticons () {
+      // 최근에 사용한 이모티콘 5개까지 클론
+      const emoticons = Array
+        .from(document.querySelectorAll('.scroll_area.recent_emoticon.recent_emoticon_default > span > a'))
+        .slice(0, 5)
+        .map(_ => $(_).clone(true, true).on('click', () => _.click()))
+  
+      boxEmoticon.empty()
+      boxEmoticon.append(emoticons)
+    }
+  
+    // 처음에 광고 등으로 이모티콘 사용 불가능할 때 인터벌
+    const interval = setInterval(() => {
+      const disabled = btnEmoticon.parent().hasClass('off')
+      if (disabled) return
+  
+      // 잠시 최근 이모티콘 목록 열어서 1회 렌더링 하도록 처리
+      btnEmoticon.click()
+      document.querySelector('.ic_clock').click() // 최근 탭
+      btnEmoticon.click()
+
+      setTimeout(() => {
+        updateRecentEmoticons()
+  
+        // 이모티콘 팝업 열고 닫을 때마다 갱신
+        btnEmoticon.on('click', () => updateRecentEmoticons())
+      }, 500)
+  
+      clearInterval(interval)
+    }, 1000)
+  }
+
   initUI()
   watchUserIdInterval()
-
-  /*
-   * 광고 시간 지나면 자동으로 스킵하는 기능.
-   * 주석 해제 시, 아프리카 정책 위반으로 계정에 불이익을 받을 수도 있습니다.
-   */
-  // setAutoAddSkipInterval()
+  initRecentEmoticons()
+  setAutoAddSkipInterval()
 })()
